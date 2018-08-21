b451960c26c603dee1e2d29a064727852f73be3b
tinghaoma <tinghaoma@sohu-inc.com>
2018-08-21 10:16:02 +0800

    数据类型

---
+++ demo/lesson_01.py
@@ -0,0 +1,21 @@
+#!/user/bin/python
+#  -*- coding: utf-8 -*-
+"""
+    我是一个注释,数据类型
+"""
+
+print('I\'m ok.')
+
+
+print('''line1
+... line2
+... line3''')
+
+print(' True and True = ', True and True)
+print(' True and False = ', True and False)
+print(' net True = ', not True)
+
+
+print(10 / 3)
+print(10 // 3)
+print(10 % 3)
